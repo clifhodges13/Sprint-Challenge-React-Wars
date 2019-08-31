939815c685be871ca555e487c4af607a73b0d3bd
Clif <clifhodges13@gmail.com>
2019-08-31 12:25:02 -0500

    created character cards from the api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import CharacterCard from './components/CharacterCard'
+import styled from 'styled-components'
+import './App.css'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,12 +11,41 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people')
+      .then(res => {
+        setData(res.data.results)
+        console.log(res.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  const StyledContainer = styled.div `
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: flex-start;
+    align-items: center;
+    color: #333;
+
+    &:last-child {
+      justify-content: center;
+    }
+  `
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <h1 style={{fontFamily: 'StarJedi'}} className="Header">React Wars</h1>
+      <StyledContainer>
+        {data.map((char, index) => {
+          return <CharacterCard key={index} name={char.name} birth_year={char.birth_year} gender={char.gender} />
+        })}
+      </StyledContainer>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ starwars/src/components/BirthYear.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BirthYear(props) {
+  return (
+    <div>
+      <h3>Born: {props.birth_year}</h3>
+    </div>
+  )
+}
