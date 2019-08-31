14c1bd53c29fedd8378244e69ecbfd27e7cbc748
Clif <clifhodges13@gmail.com>
2019-08-31 13:24:21 -0500

    attempted to create a pagination

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import CharacterCard from './components/CharacterCard'
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap'
 import styled from 'styled-components'
 import './App.css'
 
@@ -11,11 +12,14 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([])
+  const [pagination, setPagination] = useState([])
 
   useEffect(() => {
     axios.get('https://swapi.co/api/people')
       .then(res => {
+        console.log(res.data)
+        setPagination(res.data)
         setData(res.data.results)
         console.log(res.data.results)
       })
@@ -44,6 +48,28 @@ const App = () => {
           return <CharacterCard key={index} name={char.name} birth_year={char.birth_year} gender={char.gender} />
         })}
       </StyledContainer>
+      <Pagination>
+        <PaginationItem>
+          {/* <PaginationLink first href={pagination.map((char, index) => {
+          return <CharacterCard key={index} name={char.name} birth_year={char.birth_year} gender={char.gender} />
+        })} /> */}
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink previous href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">2</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink next href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink last href="#" />
+        </PaginationItem>
+      </Pagination>
     </div>
   );
 }
